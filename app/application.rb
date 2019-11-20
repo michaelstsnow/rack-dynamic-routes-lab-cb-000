5bc3562c48557125872880b5068baa01fbcf77a5
michaelstsnow <michael.st.snow@gmail.com>
2019-11-20 06:20:34 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match("/items/")
+      item = req.path.split("/items/").last
+      if item = @@items.find{|x| x.name == item}
+        resp.write item.price
+      else
+        resp.status=400
+        resp.write "Item not found"
+      end
+    else
+      resp.status=404
+      resp.write "Route not found"
+    end
+
+    resp.finish
+
+  end
+end
