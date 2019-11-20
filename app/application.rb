5d7dede36ce42e4c6abc1dad75966bd4d44b71ac
michaelstsnow <michael.st.snow@gmail.com>
2019-11-20 06:09:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,14 +2,5 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path=="/songs/Sorry"
-      resp.write @@songs[0].artist
-    elsif req.path == "/songs/Hello"
-      resp.write @@songs[1].artist
-    end
-
-    resp.finish
-  end
   end
 end
